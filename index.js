0d3880a5dae3d1b73b4e5e470d9a5f8b57b15af5
asad <139746796+typescriptwithAsad@users.noreply.github.com>
2023-07-19 20:09:11 +0500

    Add files via upload
    assignment # 7

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+// 	Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. 
+// Be sure to enclose your operations in print statements to see the results.
+let result = 4 + 4; // use Addition operator +
+console.log(`Addition operator + : ${result}.`);
+let $result = 10 - 2; // use subtraction operator -
+console.log(`Subtraction operator - : ${$result}.`);
+let _result = 4 * 2; // use multiplication operator *
+console.log(`Multiplication operaor * : ${_result}.`);
+let result1 = 64 / 8; // use Division operator /
+console.log(`Divition operator / : ${result1}.`);
---
+++ index.ts
@@ -0,0 +1,16 @@
+// 	Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. 
+// Be sure to enclose your operations in print statements to see the results.
+
+
+let result : number = 4 + 4 ;       // use Addition operator +
+console.log(`Addition operator + : ${result}.` );
+
+let $result : number = 10 - 2 ;   // use subtraction operator -
+console.log(`Subtraction operator - : ${$result}.`);
+
+let _result : number =   4 * 2 ;   // use multiplication operator *
+console.log(`Multiplication operaor * : ${_result}.`);
+
+let result1 : number = 64 / 8;        // use Division operator /
+console.log(`Divition operator / : ${result1}.` );
+
